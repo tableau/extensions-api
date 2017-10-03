939d37b5533955dc6232ab197b64fe05ef5633be
Brendan Lee <bllee@tableau.com>
2017-10-03 14:01:40 -0700

    cleanup

--- Samples/DataSources/datasources.js
+++ Samples/DataSources/datasources.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   tableau.extensions.initializeAsync().then(function () {
+    // Since dataSource info is attached to the worksheet, we will perform
+    // one async call per worksheet to get every dataSource used in this 
+    // dashboard.  This demonstrates the use of Promise.all to combine
+    // promises together and wait for each of them to resolve.
     let dataSourceFetchPromises = [];
 
     // Maps dataSource id to dataSource so we can keep track of unique dataSources.
@@ -8,7 +12,7 @@ $(document).ready(function () {
     // To get dataSource info, first get the dashboard.
     const dashboard = tableau.extensions.dashboardContent.dashboard;
 
-    // Loop through each worksheet and get its datasources, save promise for later.
+    // Then loop through each worksheet and get its datasources, save promise for later.
     dashboard.worksheets.forEach(function (worksheet) {
       dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());;
     });
@@ -16,7 +20,7 @@ $(document).ready(function () {
     Promise.all(dataSourceFetchPromises).then(function (fetchResults) {
       fetchResults.forEach(function (dataSourcesForWorksheet) {
         dataSourcesForWorksheet.forEach(function (dataSource) {
-          if (!dashboardDataSources[dataSource.id]) {
+          if (!dashboardDataSources[dataSource.id]) { // We've already seen it, skip it.
             dashboardDataSources[dataSource.id] = dataSource;
           }
         });
@@ -25,24 +29,27 @@ $(document).ready(function () {
       buildDataSourcesTable(dashboardDataSources);
     });
   }, function (err) {
-    // Something went wrong in initialization
+    // Something went wrong in initialization.
     console.log('Error while Initializing: ' + err.toString());
   });
 });
 
+// Refreshes the a given dataSource.
 function refreshDataSource(dataSource) {
   dataSource.refreshAsync().then(function () {
     console.log(dataSource.name + ': Refreshed Successfully');
   });
 }
 
+// Displays a modal dialog with more details about a given dataSource.
 function showModal(dataSource) {
   var modal = $('#infoModal');
 
-  $('#nameDetail').html(dataSource.name);
-  $('#idDetail')[0].innerHTML = dataSource.id;
-  $('#typeDetail')[0].innerHTML = (dataSource.isExtract) ? 'Extract' : 'Live';
+  $('#nameDetail').innerHTML = dataSource.name;
+  $('#idDetail').innerHTML = dataSource.id;
+  $('#typeDetail').innerHTML = (dataSource.isExtract) ? 'Extract' : 'Live';
 
+  // Loop through every field in the dataSource and concat it to a string.
   var fieldNamesStr = '';
   dataSource.fields.forEach(function (field) {
     fieldNamesStr += field.name + ', ';
@@ -54,6 +61,8 @@ function showModal(dataSource) {
   modal.modal('show');
 }
 
+// Contructs UI that displays all the dataSources in this dashboard
+// given a mapping from dataSourceId to dataSource objects.
 function buildDataSourcesTable(dataSources) {
   // Clear the table first.
   $('#dataSourcesTable > tbody tr').remove();
@@ -69,7 +78,7 @@ function buildDataSourcesTable(dataSources) {
     let infoCell = newRow.insertCell(2);
 
     let refreshButton = document.createElement('button');
-    refreshButton.innerHTML = 'Refresh Now';
+    refreshButton.innerHTML = ('Refresh Now');
     refreshButton.type = 'button';
     refreshButton.className = 'btn btn-primary';
 
