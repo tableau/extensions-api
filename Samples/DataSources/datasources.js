e53003c15237681403302e1a0ef1bcd6cd27d4ff
Brendan Lee <bllee@tableau.com>
2017-10-04 10:37:00 -0700

    Fixed tslint errors for settings and datasources

--- Samples/DataSources/datasources.js
+++ Samples/DataSources/datasources.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   tableau.extensions.initializeAsync().then(function () {
     // Since dataSource info is attached to the worksheet, we will perform
-    // one async call per worksheet to get every dataSource used in this 
+    // one async call per worksheet to get every dataSource used in this
     // dashboard.  This demonstrates the use of Promise.all to combine
     // promises together and wait for each of them to resolve.
     let dataSourceFetchPromises = [];
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
     // Then loop through each worksheet and get its datasources, save promise for later.
     dashboard.worksheets.forEach(function (worksheet) {
-      dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());;
+      dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());
     });
 
     Promise.all(dataSourceFetchPromises).then(function (fetchResults) {
@@ -35,14 +35,14 @@ $(document).ready(function () {
 });
 
 // Refreshes the a given dataSource.
-function refreshDataSource(dataSource) {
+function refreshDataSource (dataSource) {
   dataSource.refreshAsync().then(function () {
     console.log(dataSource.name + ': Refreshed Successfully');
   });
 }
 
 // Displays a modal dialog with more details about a given dataSource.
-function showModal(dataSource) {
+function showModal (dataSource) {
   var modal = $('#infoModal');
 
   $('#nameDetail').text(dataSource.name);
@@ -63,15 +63,15 @@ function showModal(dataSource) {
 
 // Contructs UI that displays all the dataSources in this dashboard
 // given a mapping from dataSourceId to dataSource objects.
-function buildDataSourcesTable(dataSources) {
+function buildDataSourcesTable (dataSources) {
   // Clear the table first.
   $('#dataSourcesTable > tbody tr').remove();
   const dataSourcesTable = document.getElementById('dataSourcesTable').getElementsByTagName('tbody')[0];
 
   // Add an entry to the dataSources table for each dataSource.
-  for (dataSourceId in dataSources) {
+  for (let dataSourceId in dataSources) {
     const dataSource = dataSources[dataSourceId];
-    
+
     let newRow = dataSourcesTable.insertRow(dataSourcesTable.rows.length);
     let nameCell = newRow.insertCell(0);
     let refreshCell = newRow.insertCell(1);
@@ -82,17 +82,17 @@ function buildDataSourcesTable(dataSources) {
     refreshButton.type = 'button';
     refreshButton.className = 'btn btn-primary';
 
-    let refreshFunction = function() { refreshDataSource(dataSource); };
+    let refreshFunction = function () { refreshDataSource(dataSource); };
     refreshButton.addEventListener('click', refreshFunction);
 
     let infoSpan = document.createElement('span');
     infoSpan.className = 'glyphicon glyphicon-info-sign';
 
-    let showModalFunction = function() { showModal(dataSource); };
+    let showModalFunction = function () { showModal(dataSource); };
     infoSpan.addEventListener('click', showModalFunction);
 
     nameCell.innerHTML = dataSource.name;
     refreshCell.appendChild(refreshButton);
     infoCell.appendChild(infoSpan);
-  };
+  }
 }
--- Samples/Settings/settings.js
+++ Samples/Settings/settings.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
   tableau.extensions.initializeAsync().then(function () {
-
     // First, check for any saved settings and populate our UI based on them.
     buildSettingsTable(tableau.extensions.settings.getAll());
-
   }, function (err) {
     // Something went wrong in initialization
     console.log('Error while Initializing: ' + err.toString());
@@ -12,18 +10,18 @@ $(document).ready(function () {
   $('#save').click(saveSetting);
 });
 
-function eraseSetting(key, row) {
+function eraseSetting (key, row) {
   // This change won't be persisted until settings.saveAsync has been called.
   tableau.extensions.settings.erase(key);
 
   // Remove the setting from the UI immediately.
   row.remove();
-  
+
   // Save in the background, saveAsync results don't need to be handled immediately.
   tableau.extensions.settings.saveAsync().then();
 }
 
-function buildSettingsTable(settings) {
+function buildSettingsTable (settings) {
   // Clear the table first.
   $('#settingsTable > tbody tr').remove();
   const settingsTable = document.getElementById('settingsTable').getElementsByTagName('tbody')[0];
@@ -38,7 +36,7 @@ function buildSettingsTable(settings) {
     let eraseSpan = document.createElement('span');
     eraseSpan.className = 'glyphicon glyphicon-trash';
 
-    let eraseFunction = function() { eraseSetting(settingKey, newRow); };
+    let eraseFunction = function () { eraseSetting(settingKey, newRow); };
     eraseSpan.addEventListener('click', eraseFunction);
 
     keyCell.innerHTML = settingKey;
@@ -47,14 +45,14 @@ function buildSettingsTable(settings) {
   }
 }
 
-function saveSetting() {
+function saveSetting () {
   var settingKey = $('#keyInput').val();
   var settingValue = $('#valueInput').val();
 
   tableau.extensions.settings.set(settingKey, settingValue);
 
   // Save the newest settings via the settings API.
-  tableau.extensions.settings.saveAsync().then((currentSettings) => {     
+  tableau.extensions.settings.saveAsync().then((currentSettings) => {
     // This promise resolves to a list of the current settings.
     // Rebuild the UI with that new list of settings.
     buildSettingsTable(currentSettings);
