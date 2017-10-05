756bca1ac63f3c07330a09d915b1b10a5029631a
Russell Hay <me@russellhay.com>
2017-10-05 09:04:39 -0700

    moving to wrapping everything into an anonymous function

--- Samples/Parameters/parameters.js
+++ Samples/Parameters/parameters.js
@@ -1,6 +1,49 @@
-$(document).ready(function () {
-  const table = $('#parameterTable');
-  const tableBody = table.children('tbody');
+'use strict';
+
+(function () {
+  $(document).ready(function () {
+    const table = $('#parameterTable');
+    const tableBody = table.children('tbody');
+
+    // This is the entry point into the extension.  It initializes the Tableau Extensions Api, and then
+    // grabs all of the parameters in the workbook, processing each one individually.
+    tableau.extensions.initializeAsync().then(function () {
+      tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(function (parameters) {
+        parameters.forEach(function (p) {
+          p.addEventListener(tableau.TableauEventType.ParameterChanged, onParameterChange);
+          parameterRow(p).appendTo(tableBody);
+        });
+
+        // This is used to manipulate what part of the UI is visible.  If there are no parameters
+        // found, we want to give you a message to tell you that you need to add one, otherwise, we
+        // show the table we created.
+        $('#loading').addClass('hidden');
+        if (parameters.length === 0) {
+          $('#addParameterWarning').removeClass('hidden').addClass('show');
+        } else {
+          $('#parameterTable').removeClass('hidden').addClass('show');
+        }
+      });
+    });
+  });
+
+  // When the parameter is changed, we recreate the row with the updated values.  This keeps the code
+  // clean, and emulates the approach that something like React does where it "rerenders" the UI with
+  // the updated data.
+  //
+  // To avoid multiple layout processing in the browser, we build the new row unattached to the DOM,
+  // and then attach it at the very end.  This helps avoid jank.
+  function onParameterChange (parameterChangeEvent) {
+    parameterChangeEvent.getParameterAsync().then(function (param) {
+      const newRow = parameterRow(param);
+      const oldRow = $("tr[data-fieldname='" + param.id + "'");
+      oldRow.replaceWith(newRow);
+    });
+  }
+
+  //
+  // DOM creation methods
+  //
 
   // A cell in the table
   function cell (value) {
@@ -54,43 +97,4 @@ $(document).ready(function () {
 
     return row;
   }
-
-  // When the parameter is changed, we recreate the row with the updated values.  This keeps the code
-  // clean, and emulates the approach that something like React does where it "rerenders" the UI with
-  // the updated data.
-  //
-  // To avoid multiple layout processing in the browser, we build the new row unattached to the DOM,
-  // and then attach it at the very end.  This helps avoid jank.
-  function onParameterChange (parameterChangeEvent) {
-    parameterChangeEvent.getParameterAsync().then(function (param) {
-      const newRow = parameterRow(param);
-      const oldRow = $("tr[data-fieldname='" + param.id + "'");
-      oldRow.replaceWith(newRow);
-    });
-  }
-
-  // This function takes a parameter, attaches and event handler, and adds it to the parameter
-  // table.
-  function processParameter (p) {
-    p.addEventListener(tableau.TableauEventType.ParameterChanged, onParameterChange);
-    parameterRow(p).appendTo(tableBody);
-  }
-
-  // This is the entry point into the extension.  It initializes the Tableau Extensions Api, and then
-  // grabs all of the parameters in the workbook, processing each one individually.
-  tableau.extensions.initializeAsync().then(function () {
-    tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(function (parameters) {
-      parameters.forEach(processParameter);
-
-      // This is used to manipulate what part of the UI is visible.  If there are no parameters
-      // found, we want to give you a message to tell you that you need to add one, otherwise, we
-      // show the table we created.
-      $('#loading').addClass('hidden');
-      if (parameters.length === 0) {
-        $('#addParameterWarning').removeClass('hidden').addClass('show');
-      } else {
-        $('#parameterTable').removeClass('hidden').addClass('show');
-      }
-    });
-  });
-});
+})();
