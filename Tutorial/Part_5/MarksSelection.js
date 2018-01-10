502eb8839f711cffae2a29ff45563c9033ae5497
Unknown <sdesmond46@gmail.com>
2018-01-10 11:50:46 -0800

    Fix some merge issues

--- Tutorial/Part_5/MarksSelection.js
+++ Tutorial/Part_5/MarksSelection.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Wrap everything in an anonymous function to avoid poluting the global namespace
+// Wrap everything in an anonymous function to avoid polluting the global namespace
 (function () {
   // Use the jQuery document ready signal to know when everything has been initialized
   $(document).ready(function () {
