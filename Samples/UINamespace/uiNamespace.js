5d5dc248a907c78a263a6dcbc689807fa9df1253
Brett Taylor <btaylor@tableau.com>
2019-08-09 13:31:48 -0700

    add updated documentation and TypeScript samples; include extensions-api-types 1.3 and new extensions-api 1.3.

--- Samples/UINamespace/uiNamespace.js
+++ Samples/UINamespace/uiNamespace.js
@@ -2,13 +2,13 @@
 
 /**
  * UINamespace Sample Extension
- * 
+ *
  * This sample extension demonstrates how to use the UI namespace
  * to create a popup dialog with additional UI that the user can interact with.
- * The content in this dialog is actually an extension as well (see the 
- * uiNamespaceDialog.js for details).  
- * 
- * This sample is an extension that auto refreshes datasources in the background of 
+ * The content in this dialog is actually an extension as well (see the
+ * uiNamespaceDialog.js for details).
+ *
+ * This sample is an extension that auto refreshes datasources in the background of
  * a dashboard.  The extension has little need to take up much dashboard space, except
  * when the user needs to adjust settings, so the UI namespace is used for that.
  */
@@ -16,7 +16,6 @@
 // Wrap everything in an anonymous function to avoid polluting the global namespace
 (function () {
   const defaultIntervalInMin = '5';
-  let refreshInterval;
   let activeDatasourceIdList = [];
 
   $(document).ready(function () {
@@ -25,30 +24,30 @@
     // item to the manifest, will add a new "Configure..." context menu item to the zone of extension
     // inside a dashboard.  When that context menu item is clicked by the user, the function passed
     // here will be executed.
-    tableau.extensions.initializeAsync({'configure': configure}).then(function() {     
+    tableau.extensions.initializeAsync({'configure': configure}).then(function () {
       // This event allows for the parent extension and popup extension to keep their
       // settings in sync.  This event will be triggered any time a setting is
       // changed for this extension, in the parent or popup (i.e. when settings.saveAsync is called).
       tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, (settingsEvent) => {
-        updateExtensionBasedOnSettings(settingsEvent.newSettings)
+        updateExtensionBasedOnSettings(settingsEvent.newSettings);
       });
     });
   });
 
-  function configure() {
-    // This uses the window.location.origin property to retrieve the scheme, hostname, and 
+  function configure () {
+    // This uses the window.location.origin property to retrieve the scheme, hostname, and
     // port where the parent extension is currently running, so this string doesn't have
     // to be updated if the extension is deployed to a new location.
     const popupUrl = `${window.location.origin}/Samples/UINamespace/uiNamespaceDialog.html`;
 
     /**
      * This is the API call that actually displays the popup extension to the user.  The
-     * popup is always a modal dialog.  The only required parameter is the URL of the popup,  
+     * popup is always a modal dialog.  The only required parameter is the URL of the popup,
      * which must be the same domain, port, and scheme as the parent extension.
-     * 
-     * The developer can optionally control the initial size of the extension by passing in 
+     *
+     * The developer can optionally control the initial size of the extension by passing in
      * an object with height and width properties.  The developer can also pass a string as the
-     * 'initial' payload to the popup extension.  This payload is made available immediately to 
+     * 'initial' payload to the popup extension.  This payload is made available immediately to
      * the popup extension.  In this example, the value '5' is passed, which will serve as the
      * default interval of refresh.
      */
@@ -64,9 +63,9 @@
     }).catch((error) => {
       // One expected error condition is when the popup is closed by the user (meaning the user
       // clicks the 'X' in the top right of the dialog).  This can be checked for like so:
-      switch(error.errorCode) {
+      switch (error.errorCode) {
         case tableau.ErrorCodes.DialogClosedByUser:
-          console.log("Dialog was closed by user");
+          console.log('Dialog was closed by user');
           break;
         default:
           console.error(error.message);
@@ -78,25 +77,25 @@
    * This function sets up a JavaScript interval based on the time interval selected
    * by the user.  This interval will refresh all selected datasources.
    */
-  function setupRefreshInterval(interval) {
-    refreshInterval = setInterval(function() { 
+  function setupRefreshInterval (interval) {
+    setInterval(function () {
       let dashboard = tableau.extensions.dashboardContent.dashboard;
       dashboard.worksheets.forEach(function (worksheet) {
         worksheet.getDataSourcesAsync().then(function (datasources) {
           datasources.forEach(function (datasource) {
-             if (activeDatasourceIdList.indexOf(datasource.id) >= 0) {
-               datasource.refreshAsync();
-             }
+            if (activeDatasourceIdList.indexOf(datasource.id) >= 0) {
+              datasource.refreshAsync();
+            }
           });
         });
       });
-    }, interval*60*1000);
+    }, interval * 60 * 1000);
   }
 
   /**
    * Helper that is called to set state anytime the settings are changed.
    */
-  function updateExtensionBasedOnSettings(settings) {
+  function updateExtensionBasedOnSettings (settings) {
     if (settings.selectedDatasources) {
       activeDatasourceIdList = JSON.parse(settings.selectedDatasources);
       $('#datasourceCount').text(activeDatasourceIdList.length);
