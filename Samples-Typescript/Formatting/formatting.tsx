a5a0ff690fcccd3dcc5545915e3ac137f4c462e3
Brian <bcarver@tableau.com>
2021-10-20 14:42:03 -0700

    dashboard object visibility and other samples

--- Samples-Typescript/Formatting/formatting.tsx
+++ Samples-Typescript/Formatting/formatting.tsx
@@ -1,49 +1,66 @@
+import { TableauEvent } from '@tableau/extensions-api-types';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 // Wrap everything in an anonymous function to avoid polluting the global namespace
 (async () => {
+  interface IFormattingState {
+    formattingUpdated: number;
+  }
 
-  class Formatting {
-    // This is the entry point into the extension.  It initializes the Tableau Extensions Api, and then
-    // will create elements with appropriate class names to get Tableau formatting
-    public async initialize() {
-      console.log('Initializing extension API');
-      await tableau.extensions.initializeAsync();
-      await this.Render();
+  class Formatting extends React.PureComponent<{}, IFormattingState> {
+    public constructor(props = {}) {
+      super(props);
+      this.state = { formattingUpdated: 0 };
+      tableau.extensions.dashboardContent.dashboard.addEventListener(tableau.TableauEventType.WorkbookFormattingChanged,
+        (event) => this.onWorkbookFormattingChanged(event));
     }
 
-    private async Render() {
+    public static async initializeAndRender(): Promise<void> {
+      // This is the entry point into the extension.  It initializes the Tableau Extensions Api, and then
+      // will create elements with appropriate class names to get Tableau formatting
+      console.log('Initializing extension API');
+      await tableau.extensions.initializeAsync();
+      ReactDOM.render(<Formatting></Formatting>, document.getElementById('formattingExample'));
+    }
+
+    private onWorkbookFormattingChanged(event: TableauEvent): void {
+      this.setState(prevState => {
+        return { formattingUpdated: prevState.formattingUpdated + 1 };
+      });
+    }
+
+    public render(): JSX.Element {
       if (tableau.extensions.environment.workbookFormatting) {
-        await this.RenderFormatting();
+        return this.renderFormatting();
       } else {
-        await this.RenderInfoMissing();
+        return this.renderInfoMissing();
       }
     }
 
-    private async RenderFormatting() {
+    private renderFormatting(): JSX.Element {
       const tooltipClassNames = 'tooltiptext ' + tableau.ClassNameKey.Tooltip;
+      const formattingChangedText = 'WorkbookFormattingChanged called ' + this.state.formattingUpdated + ' times';
 
-      ReactDOM.render(<>
+      return <>
         <h2 className={tableau.ClassNameKey.WorksheetTitle}>Subheader, using tableau-worksheet-title class</h2>
         <text className={tableau.ClassNameKey.Worksheet}>Text, using tableau-worksheet class</text>
         <h3 className='tooltip-header'>Hover to see tooltip text, which is using tableau-tooltip class
-            <span className={tooltipClassNames}>Tooltip text, using tableau-tooltip class</span>
+          <span className={tooltipClassNames}>Tooltip text, using tableau-tooltip class</span>
         </h3>
         <li className={tableau.ClassNameKey.StoryTitle}>Bullet Point, using tableau-story-title class</li>
         <text className={tableau.ClassNameKey.DashboardTitle}>Text, using tableau-dashboard-title class</text>
-      </>, document.getElementById('formattingExample'));
+        <text className={tableau.ClassNameKey.DashboardTitle}>{formattingChangedText}</text>
+      </>;
     }
 
-    private async RenderInfoMissing() {
+    private renderInfoMissing(): JSX.Element {
       const message = 'Tableau formatting information for extensions is missing from this version of Tableau.';
 
-      ReactDOM.render(<>
-        <div style={{color: 'blue'}}>{message}</div>
-      </>, document.getElementById('formattingExample'));
+      return <div style={{ color: 'blue' }}>{message}</div>;
     }
   }
 
   console.log('Initializing Formatting extension.');
-  await new Formatting().initialize();
+  await Formatting.initializeAndRender();
 })();
