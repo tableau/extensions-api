aba48adfd4ad56cd0ac9228f8e078b8091b57071
Dave Hagen <dhagen@salesforce.com>
2024-10-22 16:19:11 -0700

    removing trailingSlashes equals false config to test cause of build break

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -30,7 +30,8 @@ const getConfig = async () => {
     // indexed by search engines
     noIndex: isUpcomingVersion || isInofficial,
 
-    trailingSlash: 'false',
+   // trailing slashes cause relative links to break with URL links
+   // trailingSlash: 'false',
 
     // We want all issues to be reported as build errors
     onBrokenLinks: 'throw',
