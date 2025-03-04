85636add81b04b3294baad828cd3cfe374380095
Andy Young <andrew.young@tableau.com>
2025-03-04 13:33:00 -0800

    Show zone in authoring

--- Samples/Dashboard/BeFree/beFree.js
+++ Samples/Dashboard/BeFree/beFree.js
@@ -2,20 +2,34 @@
 
 (function () {
   let dashboardObjects = [];
+  let zoneId = -1;
 
   $(document).ready(function () {
-    tableau.extensions.initializeAsync().then(function () {
+    tableau.extensions.initializeAsync().then(async function () {
       const dashboard = tableau.extensions.dashboardContent.dashboard;
       dashboardObjects = dashboard.objects;
       console.log(dashboardObjects);
 
       $("#execute-command-btn").prop("disabled", false);
       $("#execute-command-btn").click(handleEventButtonClick);
+
+      zoneId = tableau.extensions.dashboardObjectId;
+
+      const { isPresentationMode } = await tableau.extensions.workbook.executeCommandAsync("tabui", "get-is-presentation-mode", {});
+      if (!isPresentationMode) {
+        $(".container").css("display", "block");
+
+        await tableau.extensions.workbook.executeCommandAsync("tabdoc", "set-zone-is-hidden", {
+          bool: false,
+          "zone-ids": [zoneId],
+          dashboard: dashboard.name,
+        });
+      }
+
     });
   });
 
   async function handleEventButtonClick() {
-    debugger;
     const commandNamespace = $("#namespace").val();
     const commandId = $("#command").val();
     const args = getInput();
@@ -24,11 +38,11 @@
   }
 
   function getInput() {
-    let input = undefined;
+    let input = {};
     try {
-      input = JSON.parse($("#input").val())
+      input = JSON.parse($("#input").val());
     } catch {
-      alert('Input is invalid JSON, ignoring.')
+      alert("Input is invalid JSON, ignoring.");
     }
 
     return input;
