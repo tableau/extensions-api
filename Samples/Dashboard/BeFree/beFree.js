103c0685dc87c5796650cc56ffba907d68e36aa5
Andy Young <andrew.young@tableau.com>
2025-03-04 21:34:37 -0800

    Add themes and video example

--- Samples/Dashboard/BeFree/beFree.js
+++ Samples/Dashboard/BeFree/beFree.js
@@ -15,9 +15,14 @@
       console.log(dashboardObjects);
 
       $("#execute-command-btn").prop("disabled", false);
-      $("#execute-command-btn").click(handleEventButtonClick);
       $("#execute-example-btn").prop("disabled", false);
-      $("#execute-example-btn").click(handleExampleButtonClick);
+      $("#execute-theme-btn").prop("disabled", false);
+      $("#execute-command-btn").on("click", handleEventButtonClick);
+      $("#execute-example-btn").on("click", handleExampleButtonClick);
+      $("#execute-theme-btn").on("click", handleThemeButtonClick);
+      $("#themes").on("change", (el) => (el.target.value === "custom" ? $("#custom-theme").show() : $("#custom-theme").hide()));
+
+      setupVideoListener();
 
       zoneId = api.dashboardObjectId;
 
@@ -45,124 +50,159 @@
   function handleExampleButtonClick() {
     const sample = $("#examples").val();
     switch (sample) {
-      case "grid": {
-        createGridlayout();
-      }
+      case "grid":
+        return createGridlayout();
     }
   }
 
-async function createGridlayout() {
-  await setExtensionZoneVisibility(false);
-  const sheetListItems = await getSheetList();
-  const gridSize = Math.ceil(Math.sqrt(sheetListItems.length));
+  function handleThemeButtonClick() {
+    const theme = $("#themes").val();
+    if (theme === "custom") {
+      applyThemeJson($("#custom-theme").val());
+    } else {
+      applyTheme(theme);
+    }
+  }
 
-  const { width, height } = await getDashboardDimensions();
+  async function createGridlayout() {
+    await setExtensionZoneVisibility(false);
+    const sheetListItems = await getSheetList();
+    const gridSize = Math.ceil(Math.sqrt(sheetListItems.length));
 
-  let zones = dashboard.objects.map((o) => o.id);
-  await addDashboardObject("vertical");
+    const { width, height } = await getDashboardDimensions();
 
-  let newZoneId;
-  newZoneId = dashboard.objects.find((o) => o.name === "Vertical Container" && !zones.includes(o.id)).id;
-  zones = dashboard.objects.map((o) => o.id);
+    let zones = dashboard.objects.map((o) => o.id);
+    await addDashboardObject("vertical");
 
-  await distributeChildZonesEvenly(newZoneId);
-  await addDashboardObject("blank");
-
-  const blankZoneId = dashboard.objects.find((o) => o.name === "Blank" && !zones.includes(o.id)).id;
-
-  for (let row = 0; row < gridSize; row++) {
-    const dropLocationX = Math.floor(width / 2);
-    const dropLocationY = row === 0 ? height - 100 : Math.floor(height * (1 / (row + 1)));
-    await addDashboardObject("horizontal", { x: dropLocationX, y: dropLocationY });
-
-    newZoneId = dashboard.objects.find((o) => o.name === "Horizontal Container" && !zones.includes(o.id)).id;
+    let newZoneId;
+    newZoneId = dashboard.objects.find((o) => o.name === "Vertical Container" && !zones.includes(o.id)).id;
     zones = dashboard.objects.map((o) => o.id);
 
     await distributeChildZonesEvenly(newZoneId);
-  }
+    await addDashboardObject("blank");
 
-  await removeZoneFromDashboard(blankZoneId);
+    const blankZoneId = dashboard.objects.find((o) => o.name === "Blank" && !zones.includes(o.id)).id;
 
-  let index = 0;
-  const promises = [];
+    for (let row = 0; row < gridSize; row++) {
+      const dropLocationX = Math.floor(width / 2);
+      const dropLocationY = row === 0 ? height - 100 : Math.floor(height * (1 / (row + 1)));
+      await addDashboardObject("horizontal", { x: dropLocationX, y: dropLocationY });
 
-  for (let row = 0; row < gridSize; row++) {
-    for (let col = 0; col < gridSize; col++) {
-      const dropLocationX = width - 100;
-      const dropLocationY = Math.floor(height / gridSize / 2 + (row * height) / gridSize);
-      const dropLocation = { x: dropLocationX, y: dropLocationY };
-      const sheetName = sheetListItems[index++]?.name;
+      newZoneId = dashboard.objects.find((o) => o.name === "Horizontal Container" && !zones.includes(o.id)).id;
+      zones = dashboard.objects.map((o) => o.id);
 
-      const promise = sheetName
-        ? api.workbook.executeCommandAsync("tabdoc", "drop-on-dashboard", {
-            "add-as-floating": false,
-            dashboard: dashboard.name,
-            "zone-type": "viz",
-            "drop-location": dropLocation,
-            worksheet: sheetName,
-          })
-        : addDashboardObject("blank", dropLocation);
-
-      promises.push(promise);
+      await distributeChildZonesEvenly(newZoneId);
     }
+
+    await removeZoneFromDashboard(blankZoneId);
+
+    let index = 0;
+    const promises = [];
+
+    for (let row = 0; row < gridSize; row++) {
+      for (let col = 0; col < gridSize; col++) {
+        const dropLocationX = width - 100;
+        const dropLocationY = Math.floor(height / gridSize / 2 + (row * height) / gridSize);
+        const dropLocation = { x: dropLocationX, y: dropLocationY };
+        const sheetName = sheetListItems[index++]?.name;
+
+        const promise = sheetName
+          ? api.workbook.executeCommandAsync("tabdoc", "drop-on-dashboard", {
+              "add-as-floating": false,
+              dashboard: dashboard.name,
+              "zone-type": "viz",
+              "drop-location": dropLocation,
+              worksheet: sheetName,
+            })
+          : addDashboardObject("blank", dropLocation);
+
+        promises.push(promise);
+      }
+    }
+
+    await Promise.all(promises);
+
+    await setExtensionZoneVisibility(true);
   }
 
-  await Promise.all(promises);
+  async function applyTheme(theme) {
+    await api.workbook.executeCommandAsync("tabdoc", "theme", {
+      "style-theme": theme,
+    });
+  }
 
-  await setExtensionZoneVisibility(true);
-}
+  async function applyThemeJson(json) {
+    await api.workbook.executeCommandAsync("tabdoc", "apply-theme", {
+      "file-contents": json,
+      "file-name": "Custom theme from Be Free extension",
+      "should-clear": true,
+    });
+  }
 
-async function setExtensionZoneVisibility(visible) {
-  await api.workbook.executeCommandAsync("tabdoc", "set-zone-is-hidden", {
-    bool: !visible,
-    "zone-ids": [zoneId],
-    dashboard: dashboard.name,
-  });
-}
+  async function setupVideoListener() {
+    const video = document.querySelector("video");
 
-async function getSheetList() {
-  const {
-    sheetList: { sheetListItems },
-  } = await api.workbook.executeCommandAsync("tabdoc", "get-sheet-list", {
-    "include-all-hidden": false,
-    dashboard: dashboard.name,
-  });
+    const parameters = await dashboard.getParametersAsync();
+    const videoProgressParam = parameters.find((p) => p.name === "Video Progress");
 
-  return sheetListItems;
-}
+    video.ontimeupdate = async () => {
+      const totalLength = video.duration % 60;
+      const percentageCompleted = video.currentTime / totalLength;
+      await videoProgressParam.changeValueAsync(percentageCompleted);
+    };
+  }
 
-async function getDashboardDimensions() {
-  const {
-    dashboardSizePresModel: { w: width, h: height },
-  } = await api.workbook.executeCommandAsync("tabdoc", "get-dashboard-sizing", {
-    dashboard: dashboard.name,
-  });
+  async function setExtensionZoneVisibility(visible) {
+    await api.workbook.executeCommandAsync("tabdoc", "set-zone-is-hidden", {
+      bool: !visible,
+      "zone-ids": [zoneId],
+      dashboard: dashboard.name,
+    });
+  }
 
-  return { width, height };
-}
+  async function getSheetList() {
+    const {
+      sheetList: { sheetListItems },
+    } = await api.workbook.executeCommandAsync("tabdoc", "get-sheet-list", {
+      "include-all-hidden": false,
+      dashboard: dashboard.name,
+    });
 
-async function addDashboardObject(type, dropLocation) {
-  await api.workbook.executeCommandAsync("tabdoc", "add-dashboard-object", {
-    "add-as-floating": false,
-    dashboard: dashboard.name,
-    "dashboard-object-identifer": `object.${type}`,
-    ...(dropLocation && { "drop-location": dropLocation }),
-  });
-}
+    return sheetListItems;
+  }
 
-async function distributeChildZonesEvenly(zoneId) {
-  await api.workbook.executeCommandAsync("tabdoc", "toggle-distribute-child-zones-evenly", {
-    dashboard: dashboard.name,
-    "zone-id": zoneId,
-  });
-}
+  async function getDashboardDimensions() {
+    const {
+      dashboardSizePresModel: { w: width, h: height },
+    } = await api.workbook.executeCommandAsync("tabdoc", "get-dashboard-sizing", {
+      dashboard: dashboard.name,
+    });
 
-async function removeZoneFromDashboard(zoneId) {
-  await api.workbook.executeCommandAsync("tabdoc", "hide-zone", {
-    "zone-id": zoneId,
-    dashboard: dashboard.name,
-  });
-}
+    return { width, height };
+  }
+
+  async function addDashboardObject(type, dropLocation) {
+    await api.workbook.executeCommandAsync("tabdoc", "add-dashboard-object", {
+      "add-as-floating": false,
+      dashboard: dashboard.name,
+      "dashboard-object-identifer": `object.${type}`,
+      ...(dropLocation && { "drop-location": dropLocation }),
+    });
+  }
+
+  async function distributeChildZonesEvenly(zoneId) {
+    await api.workbook.executeCommandAsync("tabdoc", "toggle-distribute-child-zones-evenly", {
+      dashboard: dashboard.name,
+      "zone-id": zoneId,
+    });
+  }
+
+  async function removeZoneFromDashboard(zoneId) {
+    await api.workbook.executeCommandAsync("tabdoc", "hide-zone", {
+      "zone-id": zoneId,
+      dashboard: dashboard.name,
+    });
+  }
 
   function getInput() {
     let input = {};
